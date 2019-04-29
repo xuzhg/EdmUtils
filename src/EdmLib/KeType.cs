a2f8b1e13c82b10dd8f884a0ebec60de4b1974f3
Sam Xu <saxu@microsoft.com>
2019-04-29 11:57:45 -0700

    Clean the EdmLib

--- src/EdmLib/KeType.cs
+++ src/EdmLib/KeType.cs
@@ -1,26 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Zhigang Xu.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
 
 namespace EdmLib
 {
+    /// <summary>
+    /// Represents the "Edm.Type".
+    /// </summary>
     public abstract class KeType
     {
+        /// <summary>
+        /// Gets/sets the namespace.
+        /// </summary>
         public string Namespace { get; set; }
 
+        /// <summary>
+        /// Gets/sets the type name.
+        /// </summary>
         public string Name { get; set; }
 
-        public KeSchema DeclareSchema { get; set; }
+        /// <summary>
+        /// Gets/sets the declared schema.
+        /// </summary>
+        public KeSchema DeclaredSchema { get; set; }
     }
 
-    public class KeTypeReference
-    {
-        public KeType Type { get; set; }
-
-        public bool IsNullable { get; set; }
-
-        public bool IsCollection { get; set; }
-    }
 }
---
+++ src/EdmLib/KeTypeReference.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Zhigang Xu.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+namespace EdmLib
+{
+    /// <summary>
+    /// Represents the refernce of a type.
+    /// </summary>
+    public class KeTypeReference
+    {
+        /// <summary>
+        /// Gets/sets the type in this reference.
+        /// If it's collection, it means the elment type.
+        /// </summary>
+        public KeType Type { get; set; }
+
+        /// <summary>
+        /// Gets/sets the nullable of this reference.
+        /// If it's collection, it means the element nullable.
+        /// </summary>
+        public bool IsNullable { get; set; }
+
+        /// <summary>
+        /// Gets/sets the collection of this reference.
+        /// </summary>
+        public bool IsCollection { get; set; }
+    }
+}
