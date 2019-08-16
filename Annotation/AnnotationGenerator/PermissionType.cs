40d5dbe6fd9041f7e6f2e71b52c5998c891d43ba
Sam Xu <saxu@microsoft.com>
2019-08-15 17:21:51 -0700

    Add new AnnotationGenerator project

---
+++ Annotation/AnnotationGenerator/PermissionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnnotationGenerator
+{
+    class PermissionType
+    {
+        public string DelegateWork { get; set; }
+        public string DelegatePersonal { get; set; }
+        public string Application { get; set; }
+        public string Verb { get; set; }
+    }
+}
---
+++ Annotation/AnnotationGenerator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AnnotationGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load the permission data : Dictionary<string, PermissionType>
+            IDictionary<string, PermissionType> permissions = new Dictionary<string, PermissionType>();
+            // Key: the request Uri
+            // Value: PermissionType
+
+            // Load Edm data
+
+            // for each permission data
+            foreach (var permission in permissions)
+            {
+                // Do Uri parser
+
+                // Generator the annotation
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    /*
+     // Get /Users
+Delegated (work or school account)	User.ReadBasic.All, User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All, Directory.AccessAsUser.All
+Delegated (personal Microsoft account)	Not supported.
+Application	User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
+
+     <Annotations Target="NS.Container/Users" >
+       <Annotation Term="Org.OData.Cap...V1.ReadRestrictions">
+         <Record>
+           <PropertyValue Property="Readable" Boolean="true" />  // maybe not
+           <PropertyValue Property="ReadByKeyRestrictions" >
+             <Record>
+                <PropertyValue Property="Permissions" >
+                  <Collection>
+                    <Record>
+                      <PropertyValue Property="SchemeName" String="Delegated (work or school account)" />
+                      <PropertyValue Property="Scopes" >
+                        <Collection>
+                          <Record>
+                            <PropertyValue Property="Scope" String="User.ReadBasic.All" />
+                          </Record>
+                          <Record>
+                            <PropertyValue Property="Scope" String="User.Read.All" />
+                          </Record>
+                          ......
+                        </Collection>
+                      </PropertyValue>
+                    </Record>
+
+                    <Record>
+                      <PropertyValue Property="SchemeName" String="Application" />
+                      <PropertyValue Property="Scopes" >
+                        <Collection>
+                          <Record>
+                            <PropertyValue Property="Scope" String="User.ReadWrite.All" />
+                          </Record>
+                          <Record>
+                            <PropertyValue Property="Scope" String="User.ReadWrite.All" />
+                          </Record>
+                          ......
+                        </Collection>
+                      </PropertyValue>
+                    </Record>
+                  </Collection>
+                </PropertyValue>
+             </Record>
+           </PropertyValue>
+         </Record>
+       </Annotation>
+     </Annotations>
+     */
+}
